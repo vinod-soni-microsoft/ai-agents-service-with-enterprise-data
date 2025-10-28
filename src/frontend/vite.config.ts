042e4b2a8fd9284837590c526cf8709c318136e7
Vinod Soni <vinod.soni@microsoft.com>
2025-10-28 10:23:44 -0400

    enterprise data

---
+++ src/frontend/vite.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "~": resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+    manifest: false,
+    cssCodeSplit: false,
+    rollupOptions: {
+      input: {
+        main: "./index.html",
+      },
+      output: {
+        entryFileNames: "assets/[name].[hash].js",
+        chunkFileNames: "assets/[name].[hash].js",
+        assetFileNames: "assets/[name].[hash][extname]",
+      },
+    },
+  },
+});
